38b7792d73a9c89a5d54d41cee06ba904d77a9e9
KubaBog <80406961+KubaBog@users.noreply.github.com>
2021-09-21 20:13:09 +0200

    Add files via upload

--- src/com/autohandel/Game.java
+++ src/com/autohandel/Game.java
@@ -27,8 +27,6 @@ public class Game {
         this.mechanics.add( new Mechanic("Janusz","Kowalski",1.0,1.0,0.0));
         this.mechanics.add( new Mechanic("Marian","Kowalski",0.7,0.9,0.0));
         this.mechanics.add( new Mechanic("Adrian","Kowalski",0.5,0.8,0.02));
-        this.clients = ClientGenerator.generateMany(6);
-        this.cars = CarGenerator.generateMany(14);
         this.clients = ClientGenerator.generateMany(4);
         this.cars = CarGenerator.generateMany(10);
         this.player1 = new Player(10000000);
@@ -50,7 +48,9 @@ public class Game {
     public void update(){
         if (this.player1.balance > 20000000 ){
             System.out.println("\n\n\n\n    YOU WON IN " + this.player1.move + " MOVES\n\n");
+            System.out.println("\n\n\n\n    YOU WON IN " + this.player1.move + " MOVES\n\n\n\n");
             this.close = true;
+            return;
         }
         this.player1.printHeader();
         StateManager.update();
