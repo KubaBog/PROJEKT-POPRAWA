32092f4135c33d4bb7197a26b4cf4c61d927a075
KubaBog <80406961+KubaBog@users.noreply.github.com>
2021-09-21 19:20:54 +0200

    Game update

--- src/com/autohandel/Game.java
+++ src/com/autohandel/Game.java
@@ -6,28 +6,26 @@ import com.autohandel.people.Mechanic;
 import com.autohandel.vehicles.Car;
 import com.autohandel.vehicles.CarGenerator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-    public int turn;
-    public List<Car> cars;
+@ -14,15 +15,18 @@ public class Game {
     public List<Client> clients;
     public List<Mechanic> mechanics;
     public Player player1;
+    public boolean close;
 
     public Game(){
         this.turn = 0;
+        this.mechanics = new ArrayList<>();
         this.mechanics.add( new Mechanic("Janusz","Kowalski",1.0,1.0,0.0));
         this.mechanics.add( new Mechanic("Marian","Kowalski",0.7,0.9,0.0));
         this.mechanics.add( new Mechanic("Adrian","Kowalski",0.5,0.8,0.02));
         this.clients = ClientGenerator.generateMany(6);
         this.cars = CarGenerator.generateMany(14);
         this.player1 = new Player(10000000);
+        this.close = false;
     }
 
     public void update(){
-
-        //TODO
-    }
-
-}
--- src/com/autohandel/Main.java
+++ src/com/autohandel/Main.java
@@ -1,10 +1,20 @@
 package com.autohandel;
 
+import com.autohandel.people.Client;
+import com.autohandel.people.ClientGenerator;
+import com.autohandel.vehicles.Car;
+import com.autohandel.vehicles.CarGenerator;
+
 public class Main {
-    
     public int turn =0;
     public static void main(String[] args) {
-	// write your code here
-    }   
-}
 
+        System.out.println(CarGenerator.generateMany(10));
+        System.out.println(ClientGenerator.generateMany(10));
+
+	// write your code here
+    public static void main(String[] args) {
+        Game game = new Game();
+        while (!game.close){
+            game.update();
+        }
