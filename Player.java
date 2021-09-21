f2d61e3195d7665c395b14fcee784b205c307104
KubaBog <80406961+KubaBog@users.noreply.github.com>
2021-09-21 19:14:54 +0200

    game player

---
+++ Player.java
@@ -0,0 +1,18 @@
+package com.autohandel;
+
+import com.autohandel.vehicles.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Player {
+    public List<Car> myCars;
+    public int balance;
+    public List<String> historyOfTransactions;
+
+
+    public Player(int balance) {
+        this.myCars = new ArrayList<>();
+        this.balance = balance;
+        this.historyOfTransactions = new ArrayList<>();
+    }
+}
--- src/com/autohandel/vehicles/Vehicle.java
+++ src/com/autohandel/vehicles/Vehicle.java
@@ -1,9 +1,20 @@
 package com.autohandel.vehicles;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Vehicle {
     public int value;
-    public String brand;
     public int mileage;
     public String color;
     public String budgetSegment;
+    public List<String> historyOfRepairs;
+
+    public Vehicle( int value, int mileage,String  color,String budgetSegment){
+        this.value = value;
+        this.mileage = mileage;
+        this.color = color;
+        this.budgetSegment = budgetSegment;
+        this.historyOfRepairs = new ArrayList<>();
+    }
+
 }
